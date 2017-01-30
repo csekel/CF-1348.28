b7ce76c1d0150c779c3adb27181f18b5351f16a1
Cliff Sekel aKa InSaneDarwin <cds87@live.com>
2017-01-29 19:16:25 -0500

    Initial Commit
    Built from CoreFoundation found in
    https://github.com/apple/swift-corelibs-foundation/tree/master/CoreFound
    ation Edit

---
+++ CoreFoundation/CoreFoundation.h
@@ -0,0 +1,19 @@
+//
+//  CoreFoundation.h
+//  CoreFoundation
+//
+//  Created by InSane on 1/28/17.
+//  Copyright © 2017 PureDarwin. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for CoreFoundation.
+FOUNDATION_EXPORT double CoreFoundationVersionNumber;
+
+//! Project version string for CoreFoundation.
+FOUNDATION_EXPORT const unsigned char CoreFoundationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CoreFoundation/PublicHeader.h>
+
+
